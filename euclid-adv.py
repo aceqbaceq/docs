bae38f40bec04950ca57ae9070e824cbe2dc104c
vasya <vasya@g.com>
2024-08-22 05:24:17 +0300

    encryp

---
+++ euclid-adv.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+def bezout(a, b):
+    '''An implementation of extended Euclidean algorithm.
+    Returns integer x, y and gcd(a, b) for Bezout equation:
+        ax + by = gcd(a, b).
+    '''
+    x, xx, y, yy = 1, 0, 0, 1
+    while b:
+        print ("начало цикла")
+        print ("b=", b)
+
+        q = a // b
+        print ("q=", q)
+
+        a, b = b, a % b
+        print ("a=", a, "b=", b)
+
+        x, xx = xx, x - xx*q
+        print ("x=", x, "xx=", xx)
+
+
+        y, yy = yy, y - yy*q
+        print ("y=", y, "yy=", yy)
+
+        print ("конец цикла")
+        
+    return (x, y)
+    
+
+a=1025
+b=100500
+
+(c,d)=bezout(a,b)
+
+print ("")
+print ("a=",a, "b=", b )
+print ("(a) *", c, "+ (b) *",d,"=", a*c+b*d,"=НОД(a,b)")
+print ("x=", c, "y=", d)
+
+
